275a8ca37c387589d2878770334cae17b68e7883
Wekesa Bill <wekesabill@gmail.com>
2019-01-08 19:18:54 +0300

    [finishes #163048787]creates the tests for the api endpoints

---
+++ app/tests/v1/test_user.py
@@ -0,0 +1,25 @@
+from app import create_app
+import json
+import unittest
+
+class TestApplication(unittest.TestCase):
+    def test_create_meetup(self):
+        pass
+
+    def test_createQuestion(self):
+        pass
+
+    def test_get_specific_meetup(self):
+        pass
+
+    def test_get_meetups(self):
+        pass
+
+    def test_upvote_question(self):
+        pass
+    
+    def test_downvote_question(self):
+        pass
+
+    def test_rsvp_question(self):
+        pass
--- run.py
+++ run.py
@@ -1,2 +1,4 @@
-from app import app
+from app import create_app
+
+app = create_app()
 app.run(debug=True)
