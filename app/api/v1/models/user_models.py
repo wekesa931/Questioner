19482c9841a81a8b7ccb8538326b662df1b1cdbb
Wekesa Bill <wekesabill@gmail.com>
2019-01-09 10:40:00 +0300

    [finishes #163080119] creates the login endpoint

--- app/api/v1/models/user_models.py
+++ app/api/v1/models/user_models.py
@@ -1,15 +1,18 @@
 users = {}
 
 class UserInfo:
-    def __init__(self, details):
+    def __init__(self, details, user_id):
         self.details = details
-        
+        self.user_id = user_id
+        self.number_of_users = len(users) + 1    
     def eachUser(self):
-        number_of_users = len(users) + 1
-        db = {"id":number_of_users}
+        
+        db = {"id":self.number_of_users}
         for k, v in self.details.items():
             if len(self.details) == 0:
                 continue
             db.update({k:v})
-            users.update({number_of_users:db})
+            users.update({self.number_of_users:db})
         return users
+    def specificUser(self):
+        return(users[self.user_id])
