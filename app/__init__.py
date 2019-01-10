b895f73373447d0187854aeb011a1d099d0a682c
Wekesa Bill <wekesabill@gmail.com>
2019-01-10 14:07:17 +0300

    [finishes #163112545] added tests for the endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from config import app_config
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    
     from app.api.v1.views.user_views import mod
     app.register_blueprint(api.v1.views.user_views.mod, url_prefix='/api')
+    
     return app
--- app/tests/v1/test_user_views.py
+++ app/tests/v1/test_user_views.py
@@ -1,12 +1,12 @@
-import app.api.v1.views.user_views as app
+#import app.api.v1.views.user_views as app
 import json
 import unittest
+from app import create_app
 
 class TestApplication(unittest.TestCase):
     def setUp(self):
-        self.client = test_client()
-
-    def test_add_user(self):
+        self.app = create_app("testing")
+        self.client = self.app.test_client()
         self.users = {
                 "firstname":"bill",
                 "lastname":"wekesa",
@@ -14,18 +14,7 @@ class TestApplication(unittest.TestCase):
                 "email":"ill@wek",
                 "phoneNumber":"526",
                 "password":"tintinabulation"
-            }        
-        app.user_signup.user_info = {
-                "firstname":"bill",
-                "lastname":"wekesa",
-                "othername":"adams",
-                "email":"ill@wek",
-                "phoneNumber":"526",
-                "password":"tintinabulation"
             }
-        self.assertEqual(self.users, app.user_signup.user_info)
-        self.assertEqual(respon.status_code, 400)
-    def test_create_meetup(self):
         self.meetups = {
                 "createdOn":"12/12/2020",
                 "location":"online",
@@ -34,30 +23,41 @@ class TestApplication(unittest.TestCase):
                 "happeningOn":"12/12/2030",
                 "tags":"classes"
             }
-        app.create_meetup.meetup = {
-                "createdOn":"12/12/2020",
-                "location":"online",
-                "images":"youtube.com/images",
-                "topics":"javascript",
-                "happeningOn":"12/12/2030",
-                "tags":"classes"
+        self.questions = {
+                "createdOn":'12/12/2020',
+                "meetup":'javascript',
+                "title":'lets learn',
+                "body":'what is js',
+                "votes":'20'
             }
-        self.assertEqual(self.meetups, app.create_meetup.meetup)
+    def tearDown(self):
+        self.app.testing = False
+        self.app = None
 
-    def test_createQuestion(self):
-        pass
+    def test_add_user_successful(self):
+        response = self.client.post('/api/v1/user/auth/signup', json=self.users, content_type='application/json')
+        self.assertIn('bill', response.data)
+        self.assertEqual(response.status_code, 201)
 
-    def test_get_specific_meetup(self):
-        pass
+    def test_add_meetup_successful(self):
+        response = self.client.post('/api/v1/add_meetups', json=self.meetups, content_type='application/json')
+        self.assertIn('online', response.data)
+        self.assertEqual(response.status_code, 201)
 
-    def test_get_meetups(self):
-        pass
-
-    def test_upvote_question(self):
-        pass
+    def test_get_meetups_sucessful(self):
+        response = self.client.get('/api/v1/get_meetups')
+        self.assertIn('online', response.data)
+        self.assertEqual(response.status_code, 200)
     
-    def test_downvote_question(self):
-        pass
+    def test_get_specific_meetup_sucessful(self):
+        response = self.client.get('/api/v1/meetups/1')
+        self.assertIn('online', response.data)
+        self.assertEqual(response.status_code, 200)
 
-    def test_rsvp_question(self):
-        pass
+    def test_add_question_successful(self):
+        response = self.client.post('/api/v1/post_question', json=self.questions, content_type='application/json')
+        self.assertIn('javascript', response.data)
+        self.assertEqual(response.status_code, 201)
+
+if __name__ == '__main__':
+    unittest.main()
