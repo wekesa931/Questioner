ff1a852082cc2d96d8d4c0f697482a5110491055
Wekesa Bill <wekesabill@gmail.com>
2019-01-12 08:46:46 +0300

    [finishes #163164930] adds the tests for user-signup endpoint validation

--- app/tests/v1/test_user_views.py
+++ app/tests/v1/test_user_views.py
@@ -15,6 +15,43 @@ class TestApplication(unittest.TestCase):
                 "phoneNumber":"526",
                 "password":"Tintinabu12"
             }
+        
+        self.users_two = {
+                "firstname":"bill",
+                "lastname":"wekesa",
+                "othername":"adams",
+                "username":"wekebill",
+                "email":"ill@wek.com",
+                "phoneNumber":"526",
+                "password":"Tintinabu12"
+        }
+        self.users_three = {
+                "firstname":"bill",
+                "lastname":"wekesa",
+                "othername":"adams",
+                "username":"wekesabill",
+                "email":"ill@wek.com",
+                "phoneNumber":"526",
+                "password":"Tintinabu12"
+        }
+        self.users_four = {
+                "firstname":"bill",
+                "lastname":"wekesa",
+                "othername":"adams",
+                "username":"wekbill",
+                "email":"il@wek.com",
+                "phoneNumber":"526",
+                "password":"tintinabu"
+        }
+        self.users_five = {
+                "firstname":"bill",
+                "lastname":"wekesa",
+                "othername":"adams",
+                "username":"wekbill",
+                "email":"ilwek.com",
+                "phoneNumber":"526",
+                "password":"Tintinabu12"
+        }
         self.meetups = {
                 "location":"online",
                 "images":"youtube.com/images",
@@ -37,6 +74,34 @@ class TestApplication(unittest.TestCase):
         self.assertIn('bill', response.data)
         self.assertEqual(response.status_code, 201)
 
+    def test_if_no_user_info_provided(self):
+        users={}
+        response = self.client.post('/api/v1/user/auth/signup', json=users, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('No data found', response.data)
+    
+    def test_if_username_is_taken(self):
+        self.client.post('/api/v1/user/auth/signup', json=self.users, content_type='application/json')
+        response = self.client.post('/api/v1/user/auth/signup', json=self.users_two, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('username taken', response.data)
+
+    def test_if_email_is_taken(self):
+        self.client.post('/api/v1/user/auth/signup', json=self.users, content_type='application/json')
+        response = self.client.post('/api/v1/user/auth/signup', json=self.users_three, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email taken', response.data)
+
+    def test_if_email_is_valid(self):
+        response = self.client.post('/api/v1/user/auth/signup', json=self.users_five, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email is not valid', response.data)
+
+    def test_if_password_is_valid(self):
+        response = self.client.post('/api/v1/user/auth/signup', json=self.users_four, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('password is not valid', response.data)
+
     def test_add_meetup_successful(self):
         response = self.client.post('/api/v1/add_meetups', json=self.meetups, content_type='application/json')
         self.assertIn('online', response.data)
@@ -53,12 +118,10 @@ class TestApplication(unittest.TestCase):
         self.assertIn('online', response.data)
         self.assertEqual(response.status_code, 200)
 
-    """ def test_get_meetup_failure(self):
+    def test_get_meetup_failure(self):
         meetups = {}
-        self.client.post('/api/v1/add_meetups', json=meetups, content_type='application/json')
-        response = self.client.get('/api/v1/get_meetups')
-        self.assertIn('No body given', response.data)
-        self.assertEqual(response.status_code, 404) """
+        response = self.client.get('/api/v1/get_meetups', json=meetups, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
     
     def test_get_specific_meetup_sucessful(self):
         response = self.client.get('/api/v1/meetups/1')
