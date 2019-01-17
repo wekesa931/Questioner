5ed3a034e3784bb2f06e19d4184cbaf1215d13e2
Wekesa Bill <wekesabill@gmail.com>
2019-01-17 04:59:41 +0300

    [Feature #163252367] Adds the endpoints for sign up

--- app/api/v2/models/user_models.py
+++ app/api/v2/models/user_models.py
@@ -1,39 +1,43 @@
-users = {}
+from urllib.parse import urlparse
 
 class UserInfo:
     """ Defines the user information """
-    def __init__(self, id, firstname, lastname, othername, username, email, phoneNumber, password):
-        self.id = id
+    def __init__(self, firstname, lastname, othername, username, email, phoneNumber, password, isAdmin):
+        self.db_config = os.getenv('api_database_url')
+        self.response = urlparse(self.db_config)
+        self.config = {
+            'database': self.response.path[1:],
+            'user': self.response.username,
+            'password': self.response.password,
+            'host': self.response.hostname
+        }
         self.firstname = firstname
         self.lastname = lastname
         self.othername = othername
         self.username = username
         self.email = email
         self.phoneNumber = phoneNumber
-        self.password = password
-        self.number_of_users = len(users) + 1    
+        self.password = password 
+        self.isAdmin = isAdmin  
 
-    def eachUser(self):
-        """ Gets user information and appends to database """
-        db={
-            "id":self.number_of_users,
-            "firstname":self.firstname,
-            "lastname":self.lastname,
-            "othername":self.othername,
-            "username":self.username,
-            "email":self.email,
-            "phoneNumber":self.phoneNumber,
-            "password":self.password
-        }
-        users.update({self.number_of_users:db})
-        return users
-
-    @staticmethod
-    def get_user(id):
-        """ returns specific user """
-        return(users[id])
-
-    @staticmethod
-    def get_all_users():
-        """ returns all users """
-        return(users)
+    def add_user(self):
+        con, response = psycopg2.connect(**self.config), None
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        try:
+            query = "INSERT INTO users ( firstname, lastname, othername, username, email, phoneNumber, password, isAdmin) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING *; "
+            cur.execute(query, (
+                self.firstname, 
+                self.lastname,
+                self.othername,
+                self.username,
+                self.email,
+                self.phoneNumber,
+                self.password,
+                self.isAdmin
+            ))
+            con.commit()
+            response = cur.fetchone()
+        except Exception as e:
+            con.close()
+        con.close()
+        return response
