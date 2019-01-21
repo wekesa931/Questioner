fb45b5661d5552390075d7362d239bf5b0b7dc7c
Wekesa Bill <wekesabill@gmail.com>
2019-01-21 21:27:59 +0300

    [bug #163354032] fixes the tests in the application

---
+++ app/tests/v2/test_image.py
@@ -0,0 +1,69 @@
+from app.tests.v2.basetest import TestApplication
+import json
+
+class TestImage(TestApplication):
+    """ test the questions endpoints """
+    
+    def test_add_image_successful(self):
+        """ test if question is successfuly added """
+        self.client.post('/api/v2/user/auth/signup', json=self.users, content_type='application/json')
+        results = self.client.post('/api/v2/user/auth/login', json=self.users, content_type='application/json')
+        token = json.loads(results.data.decode())['data'][0]['token']
+        self.client.post(
+            '/api/v2/add_meetups',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.meetups,
+            content_type='application/json'
+            )
+        
+        response = self.client.post(
+            '/api/v2/meetup/1/image',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.image, 
+            content_type='application/json'
+            )
+        self.assertIn(u'my awesome image', response.data.decode())
+        self.assertEqual(response.status_code, 201)
+
+    def test_add_image_failure(self):
+        """ test if question is successfuly added """
+        image={}
+        self.client.post('/api/v2/user/auth/signup', json=self.users, content_type='application/json')
+        results = self.client.post('/api/v2/user/auth/login', json=self.users, content_type='application/json')
+        token = json.loads(results.data.decode())['data'][0]['token']
+        self.client.post(
+            '/api/v2/add_meetups',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.meetups,
+            content_type='application/json'
+            )
+        
+        response = self.client.post(
+            '/api/v2/meetup/1/image',
+            headers = dict(Authorization = "Bearer " + token),
+            json=image, 
+            content_type='application/json'
+            )
+        self.assertIn(u'No data found', response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_image_field_absent(self):
+        """ test if question is successfuly added """
+        self.client.post('/api/v2/user/auth/signup', json=self.users, content_type='application/json')
+        results = self.client.post('/api/v2/user/auth/login', json=self.users, content_type='application/json')
+        token = json.loads(results.data.decode())['data'][0]['token']
+        self.client.post(
+            '/api/v2/add_meetups',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.meetups,
+            content_type='application/json'
+            )
+        
+        response = self.client.post(
+            '/api/v2/meetup/1/image',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.image_one, 
+            content_type='application/json'
+            )
+        self.assertIn(u'image is missing a value', response.data.decode())
+        self.assertEqual(response.status_code, 400)
---
+++ app/tests/v2/test_tags.py
@@ -0,0 +1,69 @@
+from app.tests.v2.basetest import TestApplication
+import json
+
+class TestTags(TestApplication):
+    """ test the questions endpoints """
+    
+    def test_add_tags_successful(self):
+        """ test if question is successfuly added """
+        self.client.post('/api/v2/user/auth/signup', json=self.users, content_type='application/json')
+        results = self.client.post('/api/v2/user/auth/login', json=self.users, content_type='application/json')
+        token = json.loads(results.data.decode())['data'][0]['token']
+        self.client.post(
+            '/api/v2/add_meetups',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.meetups,
+            content_type='application/json'
+            )
+        
+        response = self.client.post(
+            '/api/v2/meetup/1/tags',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.tags, 
+            content_type='application/json'
+            )
+        self.assertIn(u'my tag', response.data.decode())
+        self.assertEqual(response.status_code, 201)
+
+    def test_add_tags_failure(self):
+        """ test if question is successfuly added """
+        tags={}
+        self.client.post('/api/v2/user/auth/signup', json=self.users, content_type='application/json')
+        results = self.client.post('/api/v2/user/auth/login', json=self.users, content_type='application/json')
+        token = json.loads(results.data.decode())['data'][0]['token']
+        self.client.post(
+            '/api/v2/add_meetups',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.meetups,
+            content_type='application/json'
+            )
+        
+        response = self.client.post(
+            '/api/v2/meetup/1/tags',
+            headers = dict(Authorization = "Bearer " + token),
+            json=tags, 
+            content_type='application/json'
+            )
+        self.assertIn(u'No data found', response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_tags_field_absent(self):
+        """ test if question is successfuly added """
+        self.client.post('/api/v2/user/auth/signup', json=self.users, content_type='application/json')
+        results = self.client.post('/api/v2/user/auth/login', json=self.users, content_type='application/json')
+        token = json.loads(results.data.decode())['data'][0]['token']
+        self.client.post(
+            '/api/v2/add_meetups',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.meetups,
+            content_type='application/json'
+            )
+        
+        response = self.client.post(
+            '/api/v2/meetup/1/tags',
+            headers = dict(Authorization = "Bearer " + token),
+            json=self.tag_one, 
+            content_type='application/json'
+            )
+        self.assertIn(u'tags is missing a value', response.data.decode())
+        self.assertEqual(response.status_code, 400)
