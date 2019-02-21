50c4f797e5c0f7a8252188506299750dd6a643c4
Wekesa Bill <wekesabill@gmail.com>
2019-02-21 16:08:36 +0300

    [Feature #164137926] votes on a question

---
+++ UI/static/javascript/fetch/votes.js
@@ -0,0 +1,55 @@
+function upvoteInfo(question_id){
+    const url = 'http://127.0.0.1:5000/api/v2/' + question_id + '/upvote';
+    fetch(url,{
+        method: 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${localStorage['currentuser']}`,
+            'content-Type':'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        let string = "";
+        let object = data;
+        if(object['message']=='You have already voted'){
+            let vote_modal = document.querySelector(`.modal-vote${question_id}`);
+            let message_item=`
+                <p>${object['message']}</p>
+            `
+            vote_modal.insertAdjacentHTML("afterbegin",message_item)
+        }
+        else{
+            let votes_container = document.querySelector(`.votes-holder${question_id}`);
+            votes_container.innerHTML= object['votes'];
+        }
+   })
+    .catch(error => console.log('bad request', error))
+}
+
+function downvoteInfo(question_id){
+    const url = 'http://127.0.0.1:5000/api/v2/' + question_id + '/downvote';
+    fetch(url,{
+        method: 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${localStorage['currentuser']}`,
+            'content-Type':'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        let string = "";
+        let object = data;
+        if(object['message']=='You have already voted'){
+            let vote_modal = document.querySelector(`.modal-vote${question_id}`);
+            let message_item=`
+                <p>${object['message']}</p>
+            `
+            vote_modal.insertAdjacentHTML("afterbegin",message_item)
+        }
+        else{
+            let votes_container = document.querySelector(`.votes-holder${question_id}`);
+            votes_container.innerHTML= object['votes'];
+        }
+   })
+    .catch(error => console.log('bad request', error))
+}
