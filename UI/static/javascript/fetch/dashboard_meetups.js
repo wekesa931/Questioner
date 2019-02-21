685e866ed40ac04d337bf7661b635cc3ab98aa96
Wekesa Bill <wekesabill@gmail.com>
2019-02-21 10:41:28 +0300

    [Feature #164132537] Change user password

--- UI/static/javascript/fetch/dashboard_meetups.js
+++ UI/static/javascript/fetch/dashboard_meetups.js
@@ -100,3 +100,47 @@ fetch('http://127.0.0.1:5000/api/v2/user/name',{
     
 })
 .catch(error => console.log('bad request', error))
+
+chan_psw.addEventListener('click',()=>{
+    change_pass.classList.toggle('hide');
+})
+
+function changePassword(){
+    const url = 'http://127.0.0.1:5000/api/v2/user/auth/password';
+    let form = document.forms['passwordform'];
+    let formData = new FormData(form);
+    let data = {};
+    for (let [key, prop] of formData){
+        data[key] = prop;
+    }
+    VALUE = JSON.stringify(data);
+    fetch(url,{
+        method: 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${localStorage['currentuser']}`,
+            'content-Type':'application/json'
+        },
+        body: VALUE
+    })
+    .then(response => response.json())
+    .then(data => {
+        let string = "";
+        let object = data;
+        string = object['message'];
+        if(string == 'Token is invalid' || string == 'Permission denied!'){
+            window.location = "file:///home/wekesa/Desktop/Questioner-gh-pages/UI/routes/user.html";
+        }            
+            let psw_template=`
+                <p class="err-msg">${string}</p>
+            `
+            let error_message= document.querySelector('.change-main-password')
+            error_message.insertAdjacentHTML("beforeend", psw_template)
+            
+       
+    })
+    .catch(error => console.log('bad request', error))
+}
+function logOut(){
+    localStorage.clear();
+    window.location = "file:///home/wekesa/Desktop/Questioner-gh-pages/UI/routes/user.html";
+}
